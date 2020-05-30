6e1513e71cbdb19638ee799e76b3744e1cfadea7
Roberta Soares <kestraa_2@Silvios-MacBook-Pro.local>
2020-05-30 16:27:56 -0300

    Updates

---
+++ precache-manifest.3ce592b87c9b308978e5e0f02bae9b4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8beb1b710f53ee24f394",
+    "url": "/static/js/main.8beb1b71.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "63093c4022d519d36033",
+    "url": "/static/css/2.451da9d8.chunk.css"
+  },
+  {
+    "revision": "63093c4022d519d36033",
+    "url": "/static/js/2.63093c40.chunk.js"
+  },
+  {
+    "revision": "7549623d874733a8a1dd3a79f049fef3",
+    "url": "/index.html"
+  }
+];
